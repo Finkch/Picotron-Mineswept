51182d29e123b7c2e59100351d7d290b2d2eb834
Finkch <skyler@thealdersons.org>
2024-11-03 12:12:38 -0800

    Added comments going over board creation stategems

--- board.lua
+++ board.lua
@@ -1,5 +1,26 @@
 --[[
     the board is an nxm grid. each tile is an object that encodes
     the state of that tile; whether its a bomb or its value.
+
+    the strategy (normal) to build a board is as follows:
+    * create a list of all tiles.
+    * pop a random tile; turn the tile into a bomb tile
+    * after mines have been placed, go over remaining tiles and
+        tally the count of neighbouring mines
+
+    the strategy (swept, naive) to build a baord is as follows:
+    * don't do anything to build the board.
+    * after the players selects the first tile, reveal is as 1-8
+    * after the player selects a second tile, reveal it as a bomb
+    * place mines such that the board could be valid
+
+    the strategy (swept, advanced) to build a baord is as follows:
+    * first, place a 50-50 on the board; this 50-50 is tracked
+    * as the rest of the board is built, build according to first
+        strategy, except maintining boundary conditions
+    * when the player encounters the tracked 50-50, place a mine
+        under whichever tile the user picks
+    * place mines in the tracked 50-50 in accordance; otherwise
+        reveal the board as per normal
 ]]
 
