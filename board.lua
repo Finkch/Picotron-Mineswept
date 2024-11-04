929cec61915c777d4159b27f9873348a8bfadd2c
Finkch <skyler@thealdersons.org>
2024-11-04 14:43:33 -0800

    Added fair board generation

--- board.lua
+++ board.lua
@@ -1,3 +1,4 @@
+--[[pod_format="raw",created="2024-11-04 21:31:02",modified="2024-11-04 21:33:57",revision=5]]
 --[[
     the board is an wxh grid. each tile is an object that encodes
     the state of that tile; whether its a bomb or its value.
@@ -32,11 +33,20 @@
         reveal the board as per normal
 ]]
 
+include("lib/tstr.lua")
+
+-- some constants for the checking flags
+is_mine     = 7
+is_flag     = 6
+is_reveal   = 5
+
+
+
 Board = {}
 Board.__index = Board
 Board.__type  = "board"
 
-function Board:new(w, h, fairness, oldsprites)
+function Board:new(w, h, bombs, fairness, oldsprites)
 
     -- default values
     fairness = fairness or 0
@@ -55,6 +65,8 @@ function Board:new(w, h, fairness, oldsprites)
     local b = {
         w = w,
         h = h,
+        bombs = bombs,
+        flags = 0,
         fairness = fairness,    -- 0 = two move, 1 = insidious, 2 = standard
         bs = base_sprite,       -- which sprite represents unrevealed 0
         d = d                   -- cell side length
@@ -68,14 +80,94 @@ end
 function Board:generate()
 
     -- starts by creating a grid of unrevealed zeroes
-    for i = 0, self.w do
-        for j = 0, self.h do
+    for i = 0, self.w - 1 do
+        for j = 0, self.h - 1 do
             mset(i, j, self.bs)
         end
     end
 
-
+    -- generates the board according to fairness value
+    if self.fairness == 0 then
+        self:generate_unfair()
+    elseif self.fairness == 1 then
+        self:generate_insidious()
+    else
+        self:generate_fair()
+    end
 end
+
+-- creates a regular ol' board of mineswept
+function Board:generate_fair()
+
+    -- creates a 1d list of all cells
+    local cells = {}
+    for i = 0, self.w - 1 do
+        for j = 0, self.h - 1 do
+            add(cells, {i, j})
+        end
+    end
+
+    -- adds mines to the map
+    for i = 0, self.bombs - 1 do
+
+        -- pops a random item from the list
+        local bombify = del(cells, rnd(cells))
+
+        -- turns the popped cell into a mine.
+        -- ...i forgor lua was 1-index :^(
+        mset(bombify[1], bombify[2], self.bs + 9)
+    end
+
+    -- for the remaining cells, count adjacent mines add set the cell's value
+    for i = 1, #cells do
+
+        -- counts neighbours
+        local count = 0
+
+        for dx = -1, 1 do
+            for dy = -1, 1 do
+                if not (dx == 0 and dy == 0) then   -- don't consider self
+
+                    -- check if neighbour is a mine; if so, increment count
+                    if (self:tile(cells[i][1] + dx, cells[i][2] + dy, is_mine)) count += 1
+                end
+            end
+        end
+
+        -- sets the value of the tile
+        mset(cells[i][1], cells[i][2], self.bs + count)
+    end
+end
+
+-- generates a guaranteed loss, that will take a while to uncover
+function Board:generate_insidious()
+    -- todo
+end
+
+-- lose in two moves
+function Board:generate_unfair()
+    -- todo
+end
+
+
+
+
+-- checks whether a given flag is set.
+-- if the cell is out-of-bounds, return false
+function Board:tile(x, y, f)
+
+    -- if out of bounds, return false
+    if (x < 0 or y < y or x >= self.w or y >= self.h) return false
+
+    -- return whether the flag is set.
+    -- huh, there's a bug in picotron! fget(n, f) should return the
+    -- state of flag f when f is specified. currently, it ingores the
+    -- f parameter and returns the flag bitmap regardless.
+    -- here, i unpack the flag and check it
+    return (fget(mget(x, y)) >> f) & 1 == 1
+end
+
+
 
 -- draws the map
 function Board:draw()
@@ -87,7 +179,7 @@ function Board:draw()
     end
 
     -- when using old sprites, must draw each sprite since map assumes
-    -- sprites are 16x15
+    -- sprites are 16x16
     for i = 0, self.w do
         for j = 0, self.h do
             map(i, j, i * self.d, j * self.d, 1, 1)
