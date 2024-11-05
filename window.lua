4bb169c95ccca455717917cdf97fce553dca63f9
Finkch <skyler@thealdersons.org>
2024-11-05 12:16:32 -0800

    Window num has a frame

--- window.lua
+++ window.lua
@@ -2,6 +2,8 @@
     a cobbled-together gui for mineswept
 ]]
 
+include("lib/vec.lua")
+
 Window = {}
 Window.__index = Window
 Window.__type = "window"
@@ -9,7 +11,7 @@ Window.__type = "window"
 function Window:new(windowed, width, height)
 
     -- gets defaults when in fullscreen
-    if windowed then
+    if not windowed then
         windowed = false
         width = 480
         height = 270
@@ -17,20 +19,71 @@ function Window:new(windowed, width, height)
 
     local w = {
         fullscreen = not fullscreen,
-        width = width,
-        height = height
+        w = width,
+        h = height,
+        p = 2,
+        banner_h = 24
     }
     setmetatable(w, Window)
 
     -- creates a window
     if not w.fullscreen then
         window({
-            width = w.width,
-            height = w.height,
+            width = w.w,
+            height = w.h,
             resizable = true,
             title = "mineswept"
         })
     end
+    
+    logger(w.width, "window.lua")
 
     return w
+end
+
+-- moves camera
+function Window:update()
+    cam:focus(-Vec:new(2 * self.p, self.banner_h + 2 * self.p) + cam.centre)
+end
+
+-- draws window
+function Window:draw()
+    cls()
+
+    -- absolute draws
+    self:draw_frame()
+
+    -- relative draws
+    cam()
+
+    board:draw()
+
+
+    cam(true)
+end
+
+
+-- the background and frame behind important elements
+function Window:draw_frame()
+
+    local p = self.p
+
+    -- background colours
+    rect(0, self.banner_h, self.w - 1, self.h - 1, 6)
+    rect(1, self.banner_h + 1, self.w - 2, self.h - 2, 6)
+
+    rectfill(0, 0, self.w, self.banner_h, 6)
+
+
+    -- main window frame
+    rect(p, self.banner_h + p, self.w - p - 1, self.h - p - 1, 7)
+
+    line(p, self.banner_h + p, p, self.h - p - 1, 5)
+    line(p, self.banner_h + p, self.w - p - 2, self.banner_h + p, 5)
+
+    -- header frame
+    rect(p, p, self.w - p - 1, self.banner_h - p, 7)
+
+    line(p, p, p, self.banner_h - p, 5)
+    line(p, p, self.w - p - 2, p, 5)
 end
