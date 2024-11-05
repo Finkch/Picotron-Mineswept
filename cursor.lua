122ba357017ef5ee78fde3dbe8676c5642365f11
Finkch <skyler@thealdersons.org>
2024-11-05 13:44:31 -0800

    Added method to check for types of input

--- cursor.lua
+++ cursor.lua
@@ -2,6 +2,8 @@
     allows either the mouse or arrow keys to be used as input
 ]]
 
+include("lib/vec.lua")
+
 Cursor = {}
 Cursor.__index = Cursor
 Cursor.__type = "cursor"
@@ -10,9 +12,30 @@ function Cursor:new()
 
     local c = {
         x = 0,
-        y = 0
+        y = 0,
+        mouse = true,
+        lpos = Vec:new()
     }
 
     setmetatable(c, Cursor)
     return c
+end
+
+
+function Cursor:update()
+
+    -- polls kbm
+    kbm:update()
+
+end
+
+function Cursor:keydown()
+    return kbm:held("left") or kbm:held("right") or kbm:held("up") or kbm:held("down")
+end
+
+function Cursor:mousedown()
+    return self.lpos != kbm.pos or kbm:held("lmb") or kbm:held("rmb")
+end
+
+function Cursor:input()
 end
