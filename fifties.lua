f1d3b97cbd434a9b34e064ef51c537c74511cf8a
Finkch <skyler@thealdersons.org>
2024-11-12 13:02:54 -0800

    Added methods to rotate grid (thanks, chatgpt)

--- fifties.lua
+++ fifties.lua
@@ -19,3 +19,50 @@ function Fifty:new(grid, mines)
     return f
 end
 
+
+
+-- rotates the fifty's grid to fit in other corners
+function Fifty:rotate90()
+    local grid = self.grid
+    local numRows = #grid
+    local numCols = #grid[1]
+    local newGrid = {}
+
+    for col = 1, numCols do
+        newGrid[col] = {}
+        for row = numRows, 1, -1 do
+            newGrid[col][numRows - row + 1] = grid[row][col]
+        end
+    end
+    return Fifty:new(newGrid, self.mines)
+end
+
+function Fifty:rotate180()
+    local grid = self.grid
+    local numRows = #grid
+    local numCols = #grid[1]
+    local newGrid = {}
+
+    for row = numRows, 1, -1 do
+        newGrid[numRows - row + 1] = {}
+        for col = numCols, 1, -1 do
+            newGrid[numRows - row + 1][numCols - col + 1] = grid[row][col]
+        end
+    end
+    return Fifty:new(newGrid, self.mines)
+end
+
+function Fifty:rotate270()
+    local grid = self.grid
+    local numRows = #grid
+    local numCols = #grid[1]
+    local newGrid = {}
+
+    for col = numCols, 1, -1 do
+        newGrid[numCols - col + 1] = {}
+        for row = 1, numRows do
+            newGrid[numCols - col + 1][row] = grid[row][col]
+        end
+    end
+    return Fifty:new(newGrid, self.mines)
+end
