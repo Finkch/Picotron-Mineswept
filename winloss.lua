4b4e2f0e09ac6f7e67fa328d69265fee73384239
Finkch <skyler@thealdersons.org>
2024-11-15 14:22:38 -0800

    winlosser now has its basic functionality

--- winloss.lua
+++ winloss.lua
@@ -11,14 +11,50 @@ Winlosser.__type = "winlosser"
 
 function Winlosser:new()
 
-    local d = "appdata/minesweeper"
+    local d = "appdata/mineswept"
 
     local w = {
+        logger = Logger:new(d, false),  -- used to write to file
         d = d,                  -- directory to which to write
         f = "wl.txt",           -- file to which to write
-        logger = Logger:new(d), -- logger used to write
+        w = 0,                  -- number of wins
+        l = 0                   -- number of losses
     }
 
     setmetatable(w, Winlosser)
+
+    -- grabs w:l ratio
+    w:read()
+
     return w
+end
+
+
+-- writes to the file
+function Winlosser:write(data)
+    self.logger(data, self.f)
+end
+
+-- reads from file
+function Winlosser:read()
+
+    -- fetches the file contents
+    local contents = unlog(self.f, self.d)
+
+    -- thanks, chatgpt, for doing the regex for me
+    local w, l = contents:match("w:(%d+)%s*l:(%d+)")
+
+    -- converts from string to int
+    self.w, self.l = tonumber(w), tonumber(l)
+end
+
+-- clears the file, resetting w:l
+function Winlosser:clear()
+    self.w, self.l = 0, 0
+    self:update()
+end
+
+-- updates the file to match winlosser's w:l
+function Winlosser:update()
+    self:write(string.format("w:%d\nl:%d", self.w, self.l))
 end
