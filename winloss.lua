ad37030725b14dd235c1a6328ec2c5a81473e609
Finkch <skyler@thealdersons.org>
2024-11-15 13:55:40 -0800

    improved constructor

--- winloss.lua
+++ winloss.lua
@@ -2,12 +2,23 @@
     writes to a file to track user's win-loss ratio
 ]]
 
+include("lib/logger.lua")
+include("lib/log.lua")
+
 Winlosser = {}
 Winlosser.__index = Winlosser
 Winlosser.__type = "winlosser"
 
 function Winlosser:new()
-    local w = {}
+
+    local d = "appdata/minesweeper"
+
+    local w = {
+        d = d,                  -- directory to which to write
+        f = "wl.txt",           -- file to which to write
+        logger = Logger:new(d), -- logger used to write
+    }
+
     setmetatable(w, Winlosser)
     return w
 end
