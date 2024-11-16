95a1113fa04d8be2a63293a56823c579559ec8e1
Finkch <skyler@thealdersons.org>
2024-11-16 12:37:13 -0800

    added attributes

--- cell.lua
+++ cell.lua
@@ -6,9 +6,16 @@ Cell = {}
 Cell.__index = Cell
 Cell.__type = "cell"
 
-function Cell:new()
+function Cell:new(base_sprite)
 
-    local c = {}
+    local c = {
+        bs = base_sprite,   -- index of the first sprite in this sprite set
+        value = 0,          -- count of adjacent mines
+        flag = false,       -- is a flag
+        mine = false,       -- is a mine
+        falsy = false       -- is a false cell (aka, special flag)
+    }
+
     setmetatable(c, Cell)
     return c
 end
