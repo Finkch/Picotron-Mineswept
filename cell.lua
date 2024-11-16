58313e0da02b8158cc88839b1bd941c50457cf9f
Finkch <skyler@thealdersons.org>
2024-11-16 12:39:04 -0800

    now tracks adjacent cell

--- cell.lua
+++ cell.lua
@@ -10,12 +10,17 @@ function Cell:new(base_sprite)
 
     local c = {
         bs = base_sprite,   -- index of the first sprite in this sprite set
+        x = 0,              -- grid coordinates of cell
+        y = 0,
         value = 0,          -- count of adjacent mines
         flag = false,       -- is a flag
         mine = false,       -- is a mine
-        falsy = false       -- is a false cell (aka, special flag)
+        falsy = false,      -- is a false cell (aka, special flag)
+        adj = {}            -- list of adjacent cells
     }
 
     setmetatable(c, Cell)
     return c
 end
+
+function 
