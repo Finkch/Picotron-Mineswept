52819bf2c8998ffb94df0129aed46db68fd8a5e5
Finkch <skyler@thealdersons.org>
2024-11-16 12:53:51 -0800

    added method to update cell sprite

--- cell.lua
+++ cell.lua
@@ -10,8 +10,12 @@ function Cell:new(base_sprite)
 
     local c = {
         bs = base_sprite,   -- index of the first sprite in this sprite set
+        s = base_sprite,    -- current sprite used by cell
         x = 0,              -- grid coordinates of cell
         y = 0,
+        px = 0,             -- pixel coordinates of the cell
+        py = 0,
+        d = board.d,        -- board dimension
         value = 0,          -- count of adjacent mines
         revealed = false,   -- is revealed
         mine = false,       -- is a mine
@@ -24,6 +28,30 @@ function Cell:new(base_sprite)
     return c
 end
 
--- reveals a cell on the map
-function Cell:reveal()
+
+-- sets the sprite for the cell
+function Cell:set()
+
+    -- when cell is revealed
+    if self.revealed then
+
+        -- either cell is mine or regular cell
+        if self.mine then
+            self.s = self.bs + 9
+        else
+            self.s = self.bs + self.value
+        end
+
+    -- when the cell is flagged
+    elseif self.flag then
+        self.s = self.bs + 32
+
+    -- otherwise, unrevealed sprite
+    else
+        self.s = self.bs
+    end
+end
+
+function Cell:draw()
+    spr(self.s, self.px, self.py)
 end
