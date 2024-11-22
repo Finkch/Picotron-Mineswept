b066e7356686a83165de819c1b8082e09af39372
Finkch <skyler@thealdersons.org>
2024-11-22 12:56:16 -0800

    created fair board generation

---
+++ boards/fair.lua
@@ -0,0 +1,43 @@
+
+
+include("boards/classic.lua")
+
+FairBoard = setmetatable({}, ClassicBoard)
+FairBoard.__index = FairBoard
+FairBoard.__type = "fairboard"
+
+
+function FairBoard:new:new(fairness, oldsprites, mines, w, h)
+    local fb = ClassicBoard:new(fairness, oldsprites, mines, w, h)
+
+    setmetatable(fb, FairBoard)
+    return fb
+end
+
+
+
+-- creates a regular ol' board of mineswept.
+-- (x, y) is the position of the first reveal.
+function FairBoard:generate(x, y, mines)
+
+    -- sets false flags ensure first click reveals a zero
+    for dx = -1, 1 do
+        for dy = -1, 1 do
+            if (self:inbounds(x + dx, y + dy)) self(x + dx, y + dy):falsy()
+        end
+    end
+
+    -- places mines.
+    -- cells is the list of all cells without a mine
+    local cells = self:place_mines(mines)
+
+    -- clears any lingering false flags
+    self:all(
+        function(c) return c:falsy() end,
+        function(c) return c.is_false end,
+        cells
+    )
+
+    -- counts the value of all non-mine cells
+    self:count(cells)
+end
