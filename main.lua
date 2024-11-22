add2361befdac899eb2df4bf172f07dc251535e5
Finkch <skyler@thealdersons.org>
2024-11-22 15:16:20 -0800

    value debug print formatted to float so quantum cells don't cause crash

--- main.lua
+++ main.lua
@@ -299,7 +299,7 @@ function _update()
 
         q:add("p" .. cursor:posm():__tostring())
         q:add("b" .. Vec:new(x, y):__tostring())
-        q:add(string.format("cell value: %d", board:value(x, y)))
+        q:add(string.format("cell value: %.01f", board:value(x, y)))
         if (board.corner) q:add(string.format("ins. corner: %d", board.corner))
     end
 end
