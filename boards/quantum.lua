410797234db06283ffb732e6a8f0252e3be328cf
Finkch <skyler@thealdersons.org>
2024-11-23 14:08:56 -0800

    added board generation schema

--- boards/quantum.lua
+++ boards/quantum.lua
@@ -2,8 +2,31 @@
     a board with infinite dimensions where mines exist in all
     possible vairantions simultanously.
     defined by mine density rather than count of mines.
+
+
+    quantum/infini board generation scheme
+    * start with no grid.
+    * wherever the user first clicks and in a (Chebyshev) radius of one about the click,
+        place qcells with eigenvalues of 0 (no mine).
+    * then, building (Chebyshev) radially outward next to 0 values cells by placing 
+        qcells with eigenvalues of (0...01) corresponding to the board's density.
+        > whenever a new cell is placed, count its neighbours
+        > new cells can only be added next to cells with a count of 0
+    * stop building when there are no more 0 values cells that lack neighbours
+    * go over the frontier, wiping starting quantum data and building
+        up full quantum data for the given board
+
+
+    according to chatgpt, which in turn sites percolation theory (study of network
+    connectiveness), the smallest desnity (or critical density) to ensure the first
+    reveal is finite rather than infinite p(mine) must be greater than 0.5927 or so.
+    that's not too promising, given my default setting is 1/5, a far cry from 3/5.
+        > so, maybe as we build a board, we start at density but lerp on radius to 3/5?
+        > also, apparently infinite minesweeper is turing complete.
+
 ]]
 
+include("cells.lua")
 include("board/boards.lua")
 
 QuantumBoard = setmetatable({}, Board)
@@ -66,10 +89,6 @@ function QuantumBoard:empty()
 end
 
 
--- creates the starting board state
-function QuantumBoard:generate(x, y)
-end
-
 
 -- game actions for left click: reveal, cord
 function QuantumBoard:lclick(cursor) 
@@ -78,4 +97,51 @@ end
 
 -- game actions for right click: flag
 function QuantumBoard:rclick(cursor)
+end
+
+
+
+
+--[[
+//////////////////////////////////////////////////
+                generation methods
+//////////////////////////////////////////////////
+]]
+
+-- creates the starting board state
+function QuantumBoard:generate(x, y)
+end
+
+
+-- adds a cell to the board
+function QuantumBoard:add(x, y)
+
+    -- creates a new cell
+    local cell = QuantumCell:new(self.bs, x, y, self.d)
+
+    -- adds the cell to the map
+    self.grid[x][y] = cell
+    add(self:cells(), cell)
+
+    -- updates adjacency
+    self(x, y).adj = {}
+    for dx = -1, 1 do
+        for dy = -1, 1 do
+
+            local u, v = x + dx, y + dy
+
+            -- self is not a neighbour of self
+            if not (dx == 0 and dy == 0) and self:inbounds(u, v) then
+
+                -- adds that cell to this cell's neighbours
+                add(self(x, y).adj, self(u, v))
+
+                -- adds this cell to that cell's neihbours
+                add(self(u, v).adj, cell)
+            end
+        end
+    end
+
+    -- !! todo !!
+    -- builds frontier superpositions
 end
